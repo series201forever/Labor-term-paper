34975ffb31b3d3c17ec144ff7e9406cd9f3deaf3
Mallick Hossain <emallickhossain@users.noreply.github.com>
2015-05-22 12:43:44 -0400

    Changed tabs to spaces

--- flowu.m
+++ flowu.m
@@ -1,5 +1,5 @@
 function [flowu] = flowu(ucurve, vfir, vsec, cint, cslope, cage, wm, wf, nt, 
-												 at, dmt, dft, y, t)
+                         at, dmt, dft, y, t)
 
 % wm and wf are male and female wages.
 % nt is the number of kids
@@ -10,12 +10,13 @@ function [flowu] = flowu(ucurve, vfir, vsec, cint, cslope, cage, wm, wf, nt,
 
 if dmt == 1 && dft == 1
   flowu = ((wm + wf + y)^(1 - ucurve)) / (1 - ucurve) + 
-					vfir * nt - vsec * nt^2 - 
-					max(0, cint - cslope .* at - cage .* at .* t);
+          vfir * nt - vsec * nt^2 - 
+          max(0, cint - cslope .* at - cage .* at .* t);
 elseif dmt == 1 && dft == 0
-	flowu = ((wm + y)^(1 - ucurve)) / (1 - ucurve) + vfir * nt - vsec * nt^2;
+  flowu = ((wm + y)^(1 - ucurve)) / (1 - ucurve) + vfir * nt - vsec * nt^2;
 elseif dmt == 0 && dft == 1
-	flowu = ((wf + y)^(1 - ucurve)) / (1 - ucurve) + vfir * nt - vsec * nt^2;
+  flowu = ((wf + y)^(1 - ucurve)) / (1 - ucurve) + vfir * nt - vsec * nt^2;
 else
-	flowu = (y^(1 - ucurve)) / (1 - ucurve) + vfir * nt - vsec * nt^2;
+  flowu = (y^(1 - ucurve)) / (1 - ucurve) + vfir * nt - vsec * nt^2;
 end
+
