1256c91b86dfda2f07ad4425b07648bb2c4fd1f3
series201forever <series201forever@gmail.com>
2015-05-22 10:05:01 -0400

    Delete eps2.m

--- eps2.m
+++
@@ -1,5 +0,0 @@
-function [ eps2] = eps2(s,e,r,yh,paramat,exemp,tau,t,emaxv)
-eps2=log(max(10^(-100),(paramat{5}+paramat{6}.*yh+0.95*(emaxv(:,(1:15),(16-t)+1)-emaxv(:,(2:16),(16-t)+1))-(exemp.*tau))./(exp(paramat{1}+paramat{2}.*s+paramat{3}.*e+paramat{4}.*e.*r+paramat{9}.*e.^2).*(1-tau))));
-
-end
-
