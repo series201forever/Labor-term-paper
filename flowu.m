0f8c39d13b52a7edc10fb8524e0827db66c89b5b
Mallick Hossain <emallickhossain@users.noreply.github.com>
2015-05-22 12:41:53 -0400

    Improve Readability
    Added spacing to improve readability. Actual text of code is unchanged.

--- flowu.m
+++ flowu.m
@@ -1,14 +1,21 @@
-function [flowu] = flowu(ucurve,vfir,vsec,cint,cslope,cage,wm,wf,nt,at,dmt,dft,y,t)
-%wm,wf,nt,at as in the paper.
-%dmt=1 if male works, dft=1 if female works.
-%y outside income, t age.
-if dmt==1&&dft==1
-flowu=((wm+wf+y)^(1-ucurve))/(1-ucurve)+vfir*nt-vsec*nt^2-max(0,cint-cslope.*at-cage.*at.*t);
-elseif dmt==1&&dft==0
-flowu=((wm+y)^(1-ucurve))/(1-ucurve)+vfir*nt-vsec*nt^2;
-elseif dmt==0&&dft==1
-flowu=((wf+y)^(1-ucurve))/(1-ucurve)+vfir*nt-vsec*nt^2;
-else
-flowu=(y^(1-ucurve))/(1-ucurve)+vfir*nt-vsec*nt^2;
-end
+function [flowu] = flowu(ucurve, vfir, vsec, cint, cslope, cage, wm, wf, nt, 
+												 at, dmt, dft, y, t)
 
+% wm and wf are male and female wages.
+% nt is the number of kids
+% at is the age of the youngest kid
+% dmt and dft indicate if male or female work
+% y is the outside income
+% t is age
+
+if dmt == 1 && dft == 1
+  flowu = ((wm + wf + y)^(1 - ucurve)) / (1 - ucurve) + 
+					vfir * nt - vsec * nt^2 - 
+					max(0, cint - cslope .* at - cage .* at .* t);
+elseif dmt == 1 && dft == 0
+	flowu = ((wm + y)^(1 - ucurve)) / (1 - ucurve) + vfir * nt - vsec * nt^2;
+elseif dmt == 0 && dft == 1
+	flowu = ((wf + y)^(1 - ucurve)) / (1 - ucurve) + vfir * nt - vsec * nt^2;
+else
+	flowu = (y^(1 - ucurve)) / (1 - ucurve) + vfir * nt - vsec * nt^2;
+end
