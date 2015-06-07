0f48d1df3d98e06f00775fb349175503448a12c3
Takeaki <series201forever@gmail.com>
2015-06-07 15:35:40 -0400

    new flow utility and terminal value

---
+++ flowu_2.m
@@ -0,0 +1,24 @@
+function [flowu] = flowu_2(ucurve, vfir, vsec, cint, cslope, cageint, cageslope,...
+                        conskid, wm, wf, nt, at, dmt, dft, y, t)
+
+% wm and wf are male and female wages.
+% nt is the number of kids
+% at is the age of the youngest kid
+% dmt and dft indicate if male or female work
+% y is the outside income
+% t is age
+
+if dmt == 1 && dft == 1 && nt > 0
+  flowu = ((max(wm + wf + y - conskid*nt,0)).^(1 - ucurve)) / (1 - ucurve) + ...
+          vfir .* nt - vsec .* nt^2 - ...
+          max(0, cint + cageint .* t - cslope .* at - cageslope .* at .* t);
+elseif dmt == 1 && dft == 1 && nt == 0
+  flowu = ((wm + wf + y).^(1 - ucurve)) ./ (1 - ucurve) + vfir .* nt - vsec .* nt^2;
+elseif dmt == 1 && dft == 0
+  flowu = ((wm + y).^(1 - ucurve)) ./ (1 - ucurve) + vfir .* nt - vsec .* nt^2;
+elseif dmt == 0 && dft == 1
+  flowu = ((wf + y).^(1 - ucurve)) ./ (1 - ucurve) + vfir .* nt - vsec .* nt^2;
+else
+  flowu = (y^(1 - ucurve)) ./ (1 - ucurve) + vfir .* nt - vsec .* nt^2;
+end
+
